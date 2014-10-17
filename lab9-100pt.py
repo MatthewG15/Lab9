4b025dbc4eea60e399a14930c9203fdecf4acd10
MatthewG15 <matt@itsallgoods.us>
2014-10-17 14:15:55 -0500

    100pt

--- lab9-100pt.py
+++ lab9-100pt.py
@@ -13,3 +13,30 @@
 # - A temperature of over 105F
 # - A temperature of over 102F and they have been sick in the last 24 hours
 # - A temperature over 100, OR they've been sick in the last 24 hours, AND they've recently travelled to West Africa.
+
+print "What is the patients temperature?"
+Temp = int(raw_input())
+if Temp > 105:
+    print "send them to a hospital"
+elif Temp < 105:
+    if Temp > 102:
+        print "has the patient been sick in the past 24 hours?"
+        sickness = str(raw_input())
+        if sickness == "yes":
+            print "send them to a hospital"
+        if sickness == "no":
+            print "They are fine"
+    elif Temp > 100:
+        print "has the patient been sick in the past 24 hours?"
+        sickness2 = str(raw_input())
+        if sickness2 == "yes":
+            print "has the patient been to West Africa lately"
+            trip = str(raw_input())
+            if trip == "yes":
+                print "send them to a hospital"
+            if trip == "no":
+                print "they are fine"
+        if sickness2 == "no":
+            print "they are fine"
+    elif Temp < 100:
+        print "They are fine."
