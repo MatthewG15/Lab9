48b4c55409944a051e3d1b68cb999a0babc9cdf7
MatthewG15 <matt@itsallgoods.us>
2014-10-17 13:32:12 -0500

    70pt

--- lab9-70pt.py
+++ lab9-70pt.py
@@ -12,3 +12,10 @@
 # Accept user input 
 # Calculate fahrenheit
 # Output answer
+
+print "What is the temperature in celsius"
+Temp = int(raw_input())
+Temp = Temp * 9
+Temp = Temp / 5
+Temp = Temp + 32
+print Temp
